a2f22ca7a77180b079d16590a7414a6f5bdacb47
taksas <takuminakayama4869@gmail.com>
2022-08-04 05:43:15 +0000

    small fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
   def new
   end
 
@@ -61,6 +63,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 
 end
